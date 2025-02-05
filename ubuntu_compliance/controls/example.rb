f2396f11d6c5046508b7b49c83de2096e4d5351e
AkashKhurana3092 <89125009+AkashKhurana3092@users.noreply.github.com>
2025-02-05 19:46:10 +0530

    Update example.rb

--- ubuntu_compliance/controls/example.rb
+++ ubuntu_compliance/controls/example.rb
@@ -10,5 +10,30 @@ control 'package-01' do
   end
 end
 
+# controls/user_test.rb
 
+control 'user-01' do
+  impact 1.0
+  title 'Verify if ubuntu user exists'
+  desc 'Ensure the user ubuntu is present on the system'
 
+  describe user('ubuntu') do
+    it { should exist }
+  end
+
+  describe user('abc') do
+    it { should exist }
+  end
+end
+
+# controls/user_test.rb
+
+control 'package-02' do
+  impact 1.0
+  title 'Verify if ubuntu package exists'
+  desc 'Ensure the user ubuntu is present on the system'
+
+  describe package('dummy') do
+    it { should be_installed }
+  end
+end
